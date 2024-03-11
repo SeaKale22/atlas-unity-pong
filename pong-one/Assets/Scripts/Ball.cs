d5250986beefa924c46918a48e1db63f82dd035b
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-03-11 15:20:40 -0500

    ball script for bounce off paddle and player one paddle game object

--- pong-one/Assets/Scripts/Ball.cs
+++ pong-one/Assets/Scripts/Ball.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float ballSpeed;
+    private Rigidbody2D ballRigidbody;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // get ball's rigid body
+        ballRigidbody = GetComponent<Rigidbody2D>();
+        // launch ball in random direction
+        ballRigidbody.velocity = Random.insideUnitCircle * ballSpeed;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D Collision)
+    {
+        // Collision response to hitting a paddle, should bounce ball in opposite direction
+        if (Collision.gameObject.CompareTag("Paddle"))
+        {
+            // inverse velocity
+            ballRigidbody.velocity *= -1;
+            
+        }
+    }
+
+    void BallReset()
+    {
+        ballRigidbody.velocity = new Vector2(0, 0);
+        transform.position = new Vector3(0, 0, 0);
+        ballRigidbody.velocity = Random.insideUnitCircle * ballSpeed;
     }
 }
